3655056ab3336275f5d4ab170c90c8847a359ccc
Omar Galal Hassan Marghany <99077574+0margalal@users.noreply.github.com>
2024-05-12 19:54:29 +0300

    Update Layout.tsx

--- src/Components/Layout.tsx
+++ src/Components/Layout.tsx
@@ -1,20 +1,20 @@
-import React from "react";
-import { ReactNode } from "react";
-import Footer from './Footer'
-import Header from './Header'
+// import React from "react";
+// import { ReactNode } from "react";
+// import Footer from './Footer'
+// import Header from './Header'
 
-interface Props {
-    children: ReactNode;
-}
+// interface Props {
+//     children: ReactNode;
+// }
 
-const Layout: React.FC<Props> = ({ children }) => {
-    return (
-        <>
-            <Header />
-            {children}
-            <Footer />
-        </>
-    );
-}
+// const Layout: React.FC<Props> = ({ children }) => {
+//     return (
+//         <>
+//             <Header />
+//             {children}
+//             <Footer />
+//         </>
+//     );
+// }
 
-export default Layout;
+// export default Layout;
